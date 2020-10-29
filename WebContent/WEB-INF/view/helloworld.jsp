77201aeffde18b14ae9682afc463de5e7ae5ef8c
Artem Ubyy-Vovk <artemubyyvovk@gmail.com>
2020-10-29 09:58:21 +0100

    ProcessForm request mapping (in HelloWorldController) and corresponding html (which is "helloworld.jsp") added

---
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+Hello World of Spring!
+
+<br><br>
+
+Student name: ${param.studentName}
+
+</body>
+
+</html>
--- src/com/luv2code/springdemo/mvc/HomeController.java
+++ src/com/luv2code/springdemo/mvc/HomeController.java
@@ -1,4 +1,4 @@
-package src.com.luv2code.springdemo.mvc;
+package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
